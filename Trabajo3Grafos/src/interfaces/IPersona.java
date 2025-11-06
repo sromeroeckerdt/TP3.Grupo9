8c1bd07215fdafe2d6a5fe94eb720c009de0066f
JorgeAlava <jalavalopez@uade.edu.ar>
2025-11-05 23:16:32 -0300

    Update IPersona.java

--- Trabajo3Grafos/src/interfaces/IPersona.java
+++ Trabajo3Grafos/src/interfaces/IPersona.java
@@ -6,52 +6,52 @@ import modelo.Contacto;
 
 public interface IPersona {
 	
-	//Pre-Condición: debe estar instanciada la clase Persona teniendo nombreCompleto un valor tipo String ya asignado
-	//Post-Condición: se debe retornar el valor del atributo nombreCompleto
+	//Pre-Condición: 
+	//Post-Condición: 
 	public String getNombreCompleto();
 	
-	//Pre-Condición:debe estar instanciada la clase Persona
-	//Post-Condición: debe haber un valor tipo String en del atributo nombreCompleto
+	//Pre-Condición:
+	//Post-Condición: 
 	public void setNombreCompleto(String nombreCompleto);
 	
 	//Pre-Condición:
 	//Post-Condición:
 	public int getEdad();
 	
-	//Pre-Condición:debe estar instanciada la clase Persona
-	//Post-Condición: debe haber un valor tipo int en del atributo edad
+	//Pre-Condición:
+	//Post-Condición: 
 	public void setEdad(int edad);
 	
 	//Pre-Condición:
 	//Post-Condición:
 	public String getDni();
 	
-	//Pre-Condición:debe estar instanciada la clase Persona
-	//Post-Condición: debe haber un valor tipo String en del atributo dni
+	//Pre-Condición:
+	//Post-Condición: 
 	public void setDni(String dni);
 	
 	//Pre-Condición:
 	//Post-Condición:
 	public float getAltura();
 	
-	//Pre-Condición:debe estar instanciada la clase Persona
-	//Post-Condición: debe haber un valor tipo float en del atributo altura
+	//Pre-Condición:
+	//Post-Condición:
 	public void setAltura(float altura);
 	
 	//Pre-Condición:
 	//Post-Condición:
 	public float getPeso();
 	
-	//Pre-Condición:debe estar instanciada la clase Persona
-	//Post-Condición: debe haber un valor tipo float en del atributo peso
+	//Pre-Condición:
+	//Post-Condición:
 	public void setPeso(float peso);
 	
 	//Pre-Condición:
 	//Post-Condición:
 	public Contacto getContactos();
 	
-	//Pre-Condición:debe estar instanciada la clase Persona
-	//Post-Condición: debe haber un valor tipo Contacto en del atributo contacto
+	//Pre-Condición:
+	//Post-Condición:
 	public void setContactos(Contacto contactos);
 	
 	//Pre-Condición:
