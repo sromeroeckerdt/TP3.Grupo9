4b8e3878b4520b5cfe31de1b7544296f8084b9c9
JorgeAlava <jalavalopez@uade.edu.ar>
2025-11-05 23:13:31 -0300

    Update INodo.java
    Pre y post condiciones agregadas

--- Trabajo3Grafos/src/interfaces/INodo.java
+++ Trabajo3Grafos/src/interfaces/INodo.java
@@ -2,15 +2,15 @@ package interfaces;
 
 public interface INodo<T> {
 	
-	//Pre-Condición:
-	//Post-Condición:
+	//Pre-Condición: No requiere nada, basta con que el nodo exista en el grafo
+	//Post-Condición: Devuelve el valor actualmente almacenado en el nodo
     T getValor();
     
-    //Pre-Condición:
-  	//Post-Condición:
+    //Pre-Condición: Se recibe un nuevo valor que se desea guardar dentro del nodo, no es necesario que cumpla un formato específico
+  	//Post-Condición: El nodo actualiza su valor interno con el dato recibido, no se altera la lista de vecinos del nodo
     void setValor(T valor);
     
-    //Pre-Condición:
-  	//Post-Condición:
+    //Pre-Condición: Se recibe un nodo vecino que se desea asociar con este nodo, debe existir un nodo válido como parametro (no nulo)
+  	//Post-Condición: Si el vecino no estaba en la lista, se agrega como nuevo vecino del nodo actual, si ya existia, la lista de vecinos permanece igual
     void agregarVecino(INodo vecino);
 }
